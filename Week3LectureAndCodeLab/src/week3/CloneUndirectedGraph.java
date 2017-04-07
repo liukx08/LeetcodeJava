da8d1ce332bb80f2e770481f21fc0b64850a4223
liukx08 <liu.kexi.1990@gmail.com>
2017-04-07 14:24:18 -0400

    Move from eclipse to intellij

--- Week3LectureAndCodeLab/src/week3/CloneUndirectedGraph.java
+++ Week3LectureAndCodeLab/src/week3/CloneUndirectedGraph.java
@@ -7,15 +7,15 @@ public class CloneUndirectedGraph {
 	public GraphNode clone(GraphNode node){
 		if(node==null)return node;		// base case
 		Map<GraphNode,GraphNode> map=new HashMap<>();
-		return DFShelper(node,map);
+		return DFSHelper(node,map);
 	}
 	
-	private GraphNode DFShelper(GraphNode node,Map<GraphNode,GraphNode> map){
+	private GraphNode DFSHelper(GraphNode node,Map<GraphNode,GraphNode> map){
 		map.put(node,new GraphNode(node.label));	// copy node, use map to store the copied nodes
 		for(GraphNode nei:node.neighbors){	// search node's neighbors
 			GraphNode newNei=map.get(nei);	// check if traversed before?
-			if(newNei==null)newNei=DFShelper(newNei,map);	// if no! DFS this neighbor node
-			map.get(node).neighbors.add(newNei);	// add the new neighbor node to copied node's neighbor list 
+			if(newNei==null)newNei=DFSHelper(newNei,map);	// if no! DFS this neighbor node
+			map.get(node).neighbors.add(newNei);	// add the new neighbor node to copied node's neighbor list
 		}
 		return map.get(node); // return copied node
 	}
