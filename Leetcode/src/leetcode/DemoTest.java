c17519a62b1345b0b05b94dcb813950efdc8da14
liukx08 <liu.kexi.1990@gmail.com>
2017-04-24 15:52:58 -0400

    test

--- Leetcode/src/leetcode/DemoTest.java
+++ Leetcode/src/leetcode/DemoTest.java
@@ -1,7 +1,10 @@
 package leetcode;
 
+import java.util.Random;
+
 public class DemoTest {
 	public static void main(String[] args){
-		TestSearchInsert.testDemo();
-	}	
+		Random rand = new Random();
+		System.out.println(rand.nextInt(10));
+	}
 }
---
+++ Leetcode/src/leetcode/TreeNode.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * Created by liukx08 on 4/24/2017.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
