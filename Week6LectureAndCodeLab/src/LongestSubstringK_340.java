98f3ffab89a79642697a2e3fe8e783e967f49b5b
liukx08 <liu.kexi.1990@gmail.com>
2017-04-30 12:44:48 -0400

    chasing pointers and array map

--- Week6LectureAndCodeLab/src/LongestSubstringK_340.java
+++ Week6LectureAndCodeLab/src/LongestSubstringK_340.java
@@ -5,6 +5,33 @@ import java.util.Map;
  * Created by liukx08 on 4/30/2017.
  */
 public class LongestSubstringK_340 {
+    // use array map
+    public int lengthOfLongestK(String s, int k) {
+        if(s == null || k == 0) {
+            return 0;
+        }
+        if(s.length() <= k) {
+            return s.length();
+        }
+        // count the character quantity in current substring
+        int[] count = new int[128];
+        char[] str = s.toCharArray();
+        int start = 0, maxLen = 0;
+        for(int end = 0; end < str.length; end++) {
+            // move right pointer a character further
+            if(count[str[end]]++ == 0) {
+                k--;
+            }
+            // check the validity of current substring
+            if(k < 0) {
+                while(--count[str[start++]] > 0);
+                k++;
+            }
+            // update maximum length
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+        return maxLen;
+    }
     // the same as 159
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         if(s == null || k == 0) {
