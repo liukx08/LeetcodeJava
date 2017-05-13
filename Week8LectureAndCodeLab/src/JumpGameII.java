7dcf0a949efcad8aa8d618879fd5b95f4494f02e
liukx08 <liu.kexi.1990@gmail.com>
2017-05-13 18:36:57 -0400

    union find

--- Week8LectureAndCodeLab/src/JumpGameII.java
+++ Week8LectureAndCodeLab/src/JumpGameII.java
@@ -2,5 +2,16 @@
  * Created by liukx08 on 5/13/2017.
  */
 public class JumpGameII {
-
+    public int jump(int[] nums) {
+        if(nums == null || nums.length <= 1) {
+            return 0;
+        }
+        int[] steps = new int[nums.length];
+        for(int i = 0, j = 1; i < nums.length; i++) {
+            while(i + nums[i] >= j && j < nums.length) {
+                steps[j++] = steps[i] + 1;
+            }
+        }
+        return steps[nums.length - 1];
+    }
 }
