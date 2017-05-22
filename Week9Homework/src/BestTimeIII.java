027eb1528d62ad6662d79eb7e1b924c4a05ad97c
liukx08 <liu.kexi.1990@gmail.com>
2017-05-22 14:12:39 -0400

    DP

---
+++ Week9Homework/src/BestTimeIII.java
@@ -0,0 +1,20 @@
+/**
+ * Created by liukx08 on 5/22/2017.
+ */
+public class BestTimeIII {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length < 2) {
+            return 0;
+        }
+        int[] dp = new int[prices.length];
+        for(int i = 1; i < 3; i++) {
+            int max = - prices[0];
+            for(int j = 1; j < prices.length; j++) {
+                int tmp = dp[j];
+                dp[j] = Math.max(dp[j - 1], max + prices[j]);
+                max = Math.max(max, tmp - prices[j]);
+            }
+        }
+        return dp[prices.length - 1];
+    }
+}
